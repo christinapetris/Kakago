b6a660703ba04c72d546a5e79eddcdf97bf10079
christinapetris <christina.petris@outlook.com>
2017-05-22 11:52:38 +1200

    1st commit
    I put some ar targets of the mountain and sea exhibit for now.

---
+++ Assets/url.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class url : MonoBehaviour {
+
+	public void urlLink () {
+		Application.OpenURL("http://collections.tepapa.govt.nz/search?searchTerm=mt+victoria&scope=all");
+	}
+}
